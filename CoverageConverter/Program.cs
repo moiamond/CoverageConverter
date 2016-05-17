1b3080929de4335a63ae30434f396ecf9dfe8b29
Yao-Wei Hsieh <Yao-Wei Hsieh>
2016-05-17 10:35:27 +0800

    Convert MSTest code covarage results in to XML And view through Jenkins

---
+++ CoverageConverter/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace CoverageConverter
+{
+    using System;
+    using System.Xml;
+    using System.Xml.Xsl;
+    using Microsoft.VisualStudio.Coverage.Analysis;
+    using System.Data;
+
+    // http://codetuner.blogspot.tw/2011/09/convert-mstest-code-covarage-results-in.html
+
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            string wspath = Environment.CurrentDirectory.ToString();
+            //args[0] = "data.coverage";
+            int ret = -1;
+            if (System.IO.File.Exists(args[0]))
+            {
+                CoverageInfo coverage = CoverageInfo.CreateFromFile(args[0]);
+                DataSet data = coverage.BuildDataSet(null);
+
+                //string coveragexml = wspath + @"\vstest.coveragexml";
+                string stylexslt = wspath + @"\style.xslt";
+
+                if (System.IO.File.Exists(stylexslt))
+                {
+
+                    data.WriteXml(wspath + @"\vstest.coveragexml");
+
+                    string xml = data.GetXml();
+
+                    XmlDocument xmlDocument = new XmlDocument();
+                    xmlDocument.LoadXml(xml);
+
+
+                    XslCompiledTransform myXslTransform = new XslCompiledTransform();
+                    XmlTextWriter writer = new XmlTextWriter(wspath + @"\converted.coverage.htm", null);
+
+                    myXslTransform.Load(stylexslt);
+                    myXslTransform.Transform(xmlDocument, null, writer);
+
+                    writer.Flush();
+                    writer.Close();
+                    ret = 0;
+                }
+            }
+            return ret;
+        }
+    }
+}
